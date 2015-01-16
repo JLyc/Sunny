52708b8bb7e781e084e015f0bd0370e7bcc19a58
JLyc <andrej.socha@gmail.com>
2015-01-16 13:01:24 +0100

    end of day

--- MiniApps/src/main/java/ScreenSaver/gui/Coordination.java
+++
@@ -1,9 +0,0 @@
-package ScreenSaver.gui;
-
-public interface Coordination
-{
-	public static final int NORTH_POINT = 20;
-	public static final int SOUTH_POINT = 1200;
- 	public static final int EAST_POINT = 900;
-	public static final int WEST_POINT = 20;
-}
--- SunnySences/src/main/java/neural_center/memory/initialize_memory/helpers/FileOperators.java
+++
@@ -1,8 +0,0 @@
-package neural_center.memory.initialize_memory.helpers;
-
-/**
- * Created by sochaa on 9. 12. 2014.
- */
-public enum FileOperators {
-	LOAD, SAVE
-}
